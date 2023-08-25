09ca3ba8806512f936c2cbce515016a327e6f00f
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-08-25 14:49:47 -0300

    resolvido: exercicio 2

---
+++ vetores/exercicio02/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace exercicio02
+{
+	public class Produto
+	{
+		public string Nome { get; set; }
+		public double Preco { get; set; }
+
+        public Produto(string nome, double preco)
+        {
+			Nome = nome;
+			Preco = preco;
+        }
+    }
+}
+
