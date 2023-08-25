eeace0fe3790f1e93ce686e35fbd16e7b3e671ca
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-08-25 14:25:59 -0300

    resolvido: exercicio 1

---
+++ vetores/exercicio01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace exercicio01;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        double[] vect = new double[n];
+
+        for (int i = 0; i < n; i++){
+            vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+
+        double soma = 0.0;
+
+        for(int i = 0; i < n; i++)
+        {
+            soma += vect[i];
+        }
+
+        double media = soma / n;
+
+        Console.Write("Média das alturas: " + media.ToString("F2", CultureInfo.InvariantCulture));
+    }
+}
+
