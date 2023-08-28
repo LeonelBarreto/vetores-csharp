5000452bcb28c1a84d15018becf4db21036d2d3e
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-08-28 15:21:17 -0300

    resolvido: exercicio 3

---
+++ vetores/Exercicio03/Reserva.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Exercicio03
+{
+	public class Reserva
+	{
+		public string Nome { get; set; }
+		public string Email { get; set; }
+        
+        public Reserva(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome}, {Email}";
+        }
+    }
+}
+
